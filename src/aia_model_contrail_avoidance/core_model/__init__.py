61aca18a35f78f18bf205b38989d29ae9e9a1aa2
Ana Stojanovic <stojanovic.ana.314@gmail.com>
2026-01-16 11:21:28 +0000

    Add new functions to init file

--- src/aia_model_contrail_avoidance/core_model/__init__.py
+++ src/aia_model_contrail_avoidance/core_model/__init__.py
@@ -2,4 +2,16 @@
 
 from __future__ import annotations
 
-__all__ = ()
+from aia_model_contrail_avoidance.core_model.dimensions import (
+    SpatialGranularity,
+    TemporalGranularity,
+    _get_temporal_grouping_field,
+    _get_temporal_range_and_labels,
+)
+
+__all__ = (
+    "SpatialGranularity",
+    "TemporalGranularity",
+    "_get_temporal_grouping_field",
+    "_get_temporal_range_and_labels",
+)
