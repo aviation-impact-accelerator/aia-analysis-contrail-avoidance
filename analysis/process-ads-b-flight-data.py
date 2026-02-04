124acf03c97dfd5d87cc1f18865f478ef4c56d36
Ana Stojanovic <stojanovic.ana.314@gmail.com>
2026-02-04 15:50:36 +0000

    Update pipeline to work with adsb data

--- analysis/process-ads-b-flight-data.py
+++ analysis/process-ads-b-flight-data.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import time
+from pathlib import Path
 
 from aia_model_contrail_avoidance.flight_data_processing import (
     FlightDepartureAndArrivalSubset,
@@ -10,19 +11,29 @@ from aia_model_contrail_avoidance.flight_data_processing import (
     process_ads_b_flight_data,
 )
 
+# input directory with ADS-B data files with flight ids added
+FLIGHTS_WITH_IDS_DIR = Path("/home/as3091/ads_b_with_flight_ids")
+
+PROCESSED_FLIGHTS_WITH_IDS_DIR = Path("/home/as3091/ads_b_processed_flights")
+
+
 if __name__ == "__main__":
     start = time.time()
-    parquet_file_path = "data/flight_data/2024_01_01_sample.parquet"
-    save_filename = "2024_01_01_sample_processed_with_interpolation"
     temporal_flight_subset = TemporalFlightSubset.FIRST_MONTH
-    flight_departure_and_arrival = FlightDepartureAndArrivalSubset.UK
+    flight_departure_and_arrival = FlightDepartureAndArrivalSubset.ALL
 
-    process_ads_b_flight_data(
-        parquet_file_path,
-        save_filename,
-        flight_departure_and_arrival,
-        temporal_flight_subset,
-    )
+    parquet_file_paths = list(FLIGHTS_WITH_IDS_DIR.glob("*.parquet"))
+    for input_file in parquet_file_paths:
+        print("Processing file:", input_file.name)
+        parquet_file_path = str(input_file)
+        save_filename = input_file.stem
+
+        process_ads_b_flight_data(
+            parquet_file_path,
+            save_filename,
+            flight_departure_and_arrival,
+            temporal_flight_subset,
+        )
     end = time.time()
     length = end - start
     print("Data processing completed in", round(length / 60, 1), "minutes.")
