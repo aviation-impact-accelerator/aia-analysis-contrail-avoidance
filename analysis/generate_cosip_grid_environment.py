9de568b8f946dbb246bdc9558535c211929e0742
Ana Stojanovic <stojanovic.ana.314@gmail.com>
2026-02-04 15:35:14 +0000

    Update parameters for cosip grid

--- analysis/generate_cosip_grid_environment.py
+++ analysis/generate_cosip_grid_environment.py
@@ -8,9 +8,9 @@ from aia_model_contrail_avoidance.cosip_grid_environment import (
 
 if __name__ == "__main__":
     # Set up time and spatial bounds for the model run
-    time_bounds = ("2024-01-01 00:00:00", "2024-01-01 23:00:00")
-    lon_bounds = (-8, 3)
-    lat_bounds = (49, 62)
+    time_bounds = ("2024-01-01 00:00:00", "2024-01-07 23:00:00")
+    lon_bounds = (-120, 150)
+    lat_bounds = (-60, 60)
     # ideally would have  200 to 450 in 10 pha increments but not all levels available in ERA5
     pressure_levels = (200, 225, 250, 300, 350, 400, 450)
 
@@ -20,5 +20,5 @@ if __name__ == "__main__":
         lon_bounds=lon_bounds,
         lat_bounds=lat_bounds,
         pressure_levels=pressure_levels,
-        save_filename="cosip_grid_uk_airspace_jan01_2024",
+        save_filename="cosip_grid_global_week_1_2024.nc",
     )
