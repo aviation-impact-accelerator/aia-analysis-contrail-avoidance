5ac35e8c643268867ba9931930b649bdb553078f
Ana Stojanovic <stojanovic.ana.314@gmail.com>
2026-02-10 11:00:55 +0000

    Update distance_flown_by_flight_level_histogram to read new json format

--- plotly_analysis/plotly_distance_flown_by_flight_level_histogram.py
+++ plotly_analysis/plotly_distance_flown_by_flight_level_histogram.py
@@ -3,37 +3,42 @@
 from __future__ import annotations
 
 import json
-from typing import TYPE_CHECKING
+from pathlib import Path
 
-import numpy as np
 import plotly.express as px  # type: ignore[import-untyped]
 
-if TYPE_CHECKING:
-    from pathlib import Path
+
+def plot_distance_flown_by_flight_level_histogram_from_json(
+    stats_file_path: Path = "results/energy_forcing_statistics_week_1_2024.json",
+    output_file: str = "distance_flown_by_flight_level_histogram",
+) -> None:
+    """Plot a histogram of distance flown by flight level from a JSON statistics file.
+
+    Args:
+        stats_file_path: Path to the JSON statistics file.
+        output_file: name with which to save the output file.
+    """
+    with Path(stats_file_path).open() as f:
+        flight_statistics = json.load(f)
+        plot_distance_flown_by_flight_level_histogram(flight_statistics, output_file)
 
 
-def plot_distance_flown_by_altitude_histogram(
-    stats_file: str | Path = "2024_01_01_sample_stats_processed",
-    output_file: str | Path = "distance_flown_by_altitude_histogram",
+def plot_distance_flown_by_flight_level_histogram(
+    flight_statistics: dict,
+    output_file: str = "distance_flown_by_flight_level_histogram",
 ) -> None:
     """Plot a histogram of distance flown by flight level from statistics data.
 
     Args:
-        stats_file: Path to the JSON statistics file.
-        output_file: Path where the histogram image will be saved.
+        flight_statistics: Dictionary containing flight statistics data.
+        output_file: name with which to save the output file.
     """
-    with open(f"results/{stats_file}.json") as f:  # noqa: PTH123
-        stats = json.load(f)
-
     # Extract histogram data
-    histogram = stats["distance_flown_by_altitude_histogram"]
-    bin_edges = np.array(histogram["bin_edges"])
-    counts = np.array(histogram["distance_flown"])
-    total_distance = sum(counts)
+    histogram = flight_statistics["distance_flown_by_flight_level_histogram"]
 
     fig = px.bar(
-        x=(bin_edges[:-1] + bin_edges[1:]) / 2,
-        y=counts,
+        x=list(histogram.keys()),
+        y=list(histogram.values()),
         labels={"x": "Flight Level", "y": "Distance Flown (meters)"},
         title="Distance Flown by Flight Level Histogram",
     )
@@ -61,7 +66,7 @@ def plot_distance_flown_by_altitude_histogram(
     fig.update_yaxes(showline=True, linecolor="black", gridcolor="lightgray", mirror=True)
 
     fig.add_annotation(
-        text=f"Total Distance Flown: {total_distance:,.2f} meters",
+        text=f"Total Distance Flown: {sum(histogram.values()):,.2f} meters",
         xref="paper",
         yref="paper",  # Use relative coordinates (0-1)
         x=0.05,
@@ -81,7 +86,7 @@ def plot_distance_flown_by_altitude_histogram(
 
 
 if __name__ == "__main__":
-    plot_distance_flown_by_altitude_histogram(
-        stats_file="2024_01_01_sample_stats_processed",
-        output_file="distance_flown_by_altitude_histogram",
+    plot_distance_flown_by_flight_level_histogram_from_json(
+        stats_file_path="results/energy_forcing_statistics_week_1_2024.json",
+        output_file="distance_flown_by_flight_level_histogram",
     )
