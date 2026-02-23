fa4d8a027194c95e89cbed5fdf9d6d93029c3f88
jesseloganpalmer <jlp74@cam.ac.uk>
2026-02-23 11:22:28 +0000

    Update pressure and time variables for fine resolution

--- analysis/generate_cocip_grid_environment.py
+++ analysis/generate_cocip_grid_environment.py
@@ -2,22 +2,23 @@
 
 from __future__ import annotations
 
+import numpy as np
+
 from aia_model_contrail_avoidance.cocip_grid_environment import (
     generate_cocip_grid_environment,
 )
 
 if __name__ == "__main__":
     # Set up time and spatial bounds for the model run
-    modelling_time_bounds = ("2024-01-01 00:00:00", "2024-01-07 23:00:00")
+    modelling_time_bounds = ("2024-03-01 00:00:00", "2024-03-31 23:00:00")
     lon_bounds = (-120, 150)
     lat_bounds = (-60, 65)
-    # ideally would have  200 to 450 in 10 pha increments but not all levels available in ERA5
-    pressure_levels = (200, 225, 250, 300, 350, 400, 450)
+    pressure_levels = np.arange(150, 900, 10)
     # Generate CocipGrid environment
     generate_cocip_grid_environment(
         modelling_time_bounds=modelling_time_bounds,
         lon_bounds=lon_bounds,
         lat_bounds=lat_bounds,
         pressure_levels=pressure_levels,
-        save_filename="cocip_grid_global_week_1_coarse_pha_2024",
+        save_filename="cocip_grid_global_month_03_2024",
     )
