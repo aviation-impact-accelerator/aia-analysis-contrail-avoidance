80c8785ebbaac7853114adbb2e5793ff867b096e
Ana Stojanovic <stojanovic.ana.314@gmail.com>
2026-02-23 11:36:37 +0000

    Plot energy forcing for specific times and averages

---
+++ plotly_analysis/plot_energy_forcing.py
@@ -0,0 +1,29 @@
+"""Plot energy foricng from cocip grid."""
+
+from __future__ import annotations
+
+import logging
+
+from aia_model_contrail_avoidance.visualisation.plot_spatial_maps import (
+    plot_cocip_grid_environment,
+    plot_warming_zones_from_cocip_grid,
+)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    plot_cocip_grid_environment(
+        selected_time="2024-01-01T00:00",
+        selected_flight_level=350,
+        environment_filename="cocip_grid_global_week_1_fine_pha_2024",
+        save_filename="cocip_grid_energy_forcing_map",
+        show_uk_airspace=True,
+    )
+    plot_warming_zones_from_cocip_grid(
+        selected_time_start="2024-01-01T00:00",
+        selected_time_end="2024-01-02T00:00",
+        selected_flight_level_top=430,
+        selected_flight_level_bottom=300,
+        environment_filename="cocip_grid_global_week_1_fine_pha_2024",
+        save_filename="cocip_grid_energy_forcing_warming_zones_map",
+        show_uk_airspace=True,
+    )
